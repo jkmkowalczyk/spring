4506a8bd623da200490804ab426d53b0817a3e0a
Krzysztof Chmielewski <chmielewski.k.s@gmail.com>
2018-03-13 12:12:34 +0100

    03: request mapping example

---
+++ 02 Request Mapping/src/main/java/com/kchmielewski/sda/java6/spring02java/player/web/PlayerController.java
@@ -0,0 +1,41 @@
+package com.kchmielewski.sda.java6.spring02java.player.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Controller
+public class PlayerController {
+    private final List<String> players = new ArrayList<>();
+
+    public PlayerController() {
+        players.addAll(Arrays.asList("Adam Lallana", "Philippe Coutinho"));
+    }
+
+    @GetMapping("display")
+    public String display(Model model) {
+        model.addAttribute("players", players);
+
+        return "players";
+    }
+
+    @PostMapping("add")
+    public String add(@RequestParam String name, @RequestParam("myVeryStrangeParameterName") String surname, Model model) {
+        players.add(name + " " + surname);
+        model.addAttribute("players", players);
+
+        return "players";
+    }
+
+    @PostMapping("remove")
+    public String remove(@RequestParam String name, @RequestParam String surname, Model model) {
+        players.remove(name + " " + surname);
+        model.addAttribute("players", players);
+
+        return "players";
+    }
+}
